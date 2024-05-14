2dacf709121427518c2ce84406f61862f36b13ce
sudhir-growwork <sudhir@growworkinfotech.com>
2024-05-14 18:42:14 +0530

    Fixed: Payment intigration

--- src/views/Pages/Paymentstatus.js
+++ src/views/Pages/Paymentstatus.js
@@ -17,9 +17,10 @@ function Paymentstatus() {
     };
     fetchPaymentStatus();
   }, []);
-  const details = useSelector(
-    (state) => state?.payment?.paymentstatus?.response?.response
-  );
+  const details = useSelector((state) => state?.payment?.paymentstatus?.data);
+  console.info("-------------------------------");
+  console.info("details => ", details);
+  console.info("-------------------------------");
   const slug = localStorage.getItem("slug");
   const history = useHistory();
   return (
